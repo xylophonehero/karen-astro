c6aa51f762b79af061f6fe0279331b33ce7333de
xylophonehero <nick97000@gmail.com>
2023-10-07 17:55:42 +0100

    feat:adding some storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        section: "storyblok/Section",
+        rich_text: "storyblok/RichText",
+        testimonial_grid: "storyblok/TestimonialGrid",
+      },
+    }),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly STORYBLOK_TOKEN: string;
+}

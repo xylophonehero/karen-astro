bdaba8e1ad82390086903b8b15fdcc17cd3bd49c
xylophonehero <nick97000@gmail.com>
2023-10-08 09:23:20 +0100

    feat:add components

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ const env = loadEnv("", process.cwd(), "STORYBLOK");
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
       components: {
@@ -16,6 +18,10 @@ export default defineConfig({
         section: "storyblok/Section",
         rich_text: "storyblok/RichText",
         testimonial_grid: "storyblok/TestimonialGrid",
+        cta_block: "storyblok/CtaBlock",
+        grid: "storyblok/Grid",
+        service: "storyblok/Service",
+        contact_form: "storyblok/ContactForm",
       },
     }),
   ],
